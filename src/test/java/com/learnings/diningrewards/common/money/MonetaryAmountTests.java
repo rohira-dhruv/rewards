32538b8aa3009ca84ea63baad12e077564e001f4
dhruv rohira <drohira@wayfair.com>
2025-02-27 01:27:26 +0530

    add common models and tests

---
+++ src/test/java/com/learnings/diningrewards/common/money/MonetaryAmountTests.java
@@ -0,0 +1,63 @@
+package com.learnings.diningrewards.common.money;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit tests that make sure the MonetaryAmount class works in isolation.
+ */
+class MonetaryAmountTests {
+
+  @Test
+  void testMonetaryAmountValueOfString() {
+    MonetaryAmount amount = MonetaryAmount.valueOf("$100");
+    assertEquals("$100.00", amount.toString());
+  }
+
+  @Test
+  void testMonetaryCreation() {
+    MonetaryAmount amt = MonetaryAmount.valueOf("100.00");
+    assertEquals("$100.00", amt.toString());
+  }
+
+  @Test
+  void testMonetaryAdd() {
+    MonetaryAmount amt1 = MonetaryAmount.valueOf("100.00");
+    MonetaryAmount amt2 = MonetaryAmount.valueOf("100.00");
+    assertEquals(MonetaryAmount.valueOf("200.00"), amt1.add(amt2));
+    assertEquals("$200.00", amt1.add(amt2).toString());
+  }
+
+  @Test
+  void testMultiplyByPercentage() {
+    MonetaryAmount amt = MonetaryAmount.valueOf("100.005");
+    assertEquals(MonetaryAmount.valueOf("8.00"), amt.multiplyBy(Percentage.valueOf("8%")));
+  }
+
+  @Test
+  void testMultiplyByDecimal() {
+    MonetaryAmount amt = MonetaryAmount.valueOf("100.005");
+    assertEquals(MonetaryAmount.valueOf("8.00"), amt.multiplyBy(new BigDecimal(0.08)));
+  }
+
+  @Test
+  void testDivideByMonetaryAmount() {
+    MonetaryAmount amt = MonetaryAmount.valueOf("100.005");
+    assertEquals(new BigDecimal(12.5), amt.divide(MonetaryAmount.valueOf("8.00")));
+  }
+
+  @Test
+  void testDivideByDecimal() {
+    MonetaryAmount amt = MonetaryAmount.valueOf("100.005");
+    assertEquals(MonetaryAmount.valueOf("8.00"), amt.divideBy(new BigDecimal(12.5)));
+  }
+
+  @Test
+  void testDoubleEquality() {
+    MonetaryAmount amt = MonetaryAmount.valueOf(".1");
+    assertEquals(new BigDecimal(".10"), amt.asBigDecimal());
+  }
+}
+
---
+++ src/test/java/com/learnings/diningrewards/common/money/PercentageTests.java
@@ -0,0 +1,41 @@
+package com.learnings.diningrewards.common.money;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit tests that make sure the Percentage class works in isolation.
+ */
+class PercentageTests {
+
+  @Test
+  void testPercentageValueOfString() {
+    Percentage percentage = Percentage.valueOf("100%");
+    assertEquals("100%", percentage.toString());
+  }
+
+  @Test
+  void testPercentage() {
+    assertEquals(Percentage.valueOf("0.01"), Percentage.valueOf("1%"));
+  }
+
+  @Test
+  void testPercentageEquality() {
+    Percentage percentage1 = Percentage.valueOf("25%");
+    Percentage percentage2 = Percentage.valueOf("25%");
+    assertEquals(percentage1, percentage2);
+  }
+
+  @Test
+  void testNewPercentage() {
+    Percentage p = new Percentage(.25);
+    assertEquals("25%", p.toString());
+  }
+
+  @Test
+  void testNewPercentageWithRounding() {
+    Percentage p = new Percentage(.255555);
+    assertEquals("26%", p.toString());
+  }
+}
