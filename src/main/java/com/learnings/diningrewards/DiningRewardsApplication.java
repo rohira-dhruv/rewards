db64aa4d179516460f6c67b380e04b34e8899ad7
dhruv rohira <drohira@wayfair.com>
2025-02-27 01:11:46 +0530

    add project files

---
+++ src/main/java/com/learnings/diningrewards/DiningRewardsApplication.java
@@ -0,0 +1,13 @@
+package com.learnings.diningrewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningRewardsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiningRewardsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/learnings/diningrewards/DiningRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnings.diningrewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningRewardsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
